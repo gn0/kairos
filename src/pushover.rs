8854b9242e8870681a126647106da323ab389468
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2025-08-30 16:57:24 -0400

    implement Pushover::send

--- src/pushover.rs
+++ src/pushover.rs
@@ -5,3 +5,37 @@ pub struct Pushover {
     pub token: String,
     pub user: String,
 }
+
+impl Pushover {
+    pub async fn send(
+        &self,
+        message: &str,
+        title: Option<&str>,
+    ) -> Result<(), String> {
+        let client = reqwest::Client::new();
+        let mut form_data = vec![
+            ("token", self.token.as_str()),
+            ("user", self.user.as_str()),
+            ("message", message),
+        ];
+
+        if let Some(x) = title {
+            form_data.push(("title", x));
+        }
+
+        let status_code = client
+            .post("https://api.pushover.net/1/messages.json")
+            .form(&form_data)
+            .send()
+            .await
+            .map_err(|x| x.to_string())?
+            .status()
+            .as_u16();
+
+        if status_code == 200 {
+            Ok(())
+        } else {
+            Err(format!("pushover: status code {status_code}"))
+        }
+    }
+}
